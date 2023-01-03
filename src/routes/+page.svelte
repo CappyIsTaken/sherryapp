f5651f090344d298b71ed9f6079d6f68a360d2a9
Alen Kapitanker <alengamer76@gmail.com>
2023-01-03 20:35:05 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+
+<div class='mt-2'>
+        <h1 class="text-white font-extrabold text-8xl uppercase">This cat Isn't sherry!</h1>
+</div>
+<div class="mt-20 justify-center flex">
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <img src="https://cataas.com/cat"/>
+</div>
+<div class="mt-6">
+    <h3 class="text-white text-2xl font-semibold">Credits to Cappy (Aleph)#3287</h3>
+</div>
